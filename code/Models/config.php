2bea17b838d960dc29a21857208905b647f8522c
leno <lam.do@asiantech.vn>
2018-12-04 22:25:57 +0700

    Create class Config

---
+++ code/Models/config.php
@@ -0,0 +1,10 @@
+<?php 
+
+class Config 
+{
+    const DB_HOST = 'mysql'; 
+    const DB_USERNAME = 'root'; 
+    const DB_PASSWORD = 'root'; 
+    const DB_PORT = '3303'; 
+    const DB_NAME = 'learn_php'; 
+}
