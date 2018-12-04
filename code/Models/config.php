8b7af21b1b3cf9042ffc711555a8fa01bf459967
asiantech <dung.le2@asiantech.vn>
2018-12-04 17:11:14 +0700

    [Homework 1] Create file config.php

---
+++ code/Models/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+* 
+*/
+  class Config
+  { 
+    private $username = "root";
+    private $password = "root";
+    private $host = 'mysql';
+    private $dbname = "internship";
+    private $port = "3306"; 
+  }
