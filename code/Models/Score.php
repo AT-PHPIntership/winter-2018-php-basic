573a8ce30d09191dd5af6d4d431573c9a7cd9f1b
thanhbuiv <thanh.bui@asiantech.vn>
2018-12-05 20:19:21 +0700

    edit query skillFavorite and skillHasSecondPoint

--- code/Models/Score.php
+++ code/Models/Score.php
@@ -9,28 +9,38 @@ class Score extends Model
     private $score;
     
     function avgUser(){
-        $stmt = $this->$connect->prepare('SELECT user.name as user_name, AVG(score.score) score_avg, group_concat(skill.name) as group_skill_name FROM user JOIN score ON user.id = score.user_id LEFT JOIN skill ON score.skill_id = skill.id GROUP BY user.id');
+        $stmt = $this->$connect->prepare('SELECT user.name as user_name, AVG(score.score) score_avg, group_concat(skill.name) as group_skill_name 
+        FROM user JOIN score ON user.id = score.user_id LEFT JOIN skill ON score.skill_id = skill.id GROUP BY user.id');
         $stmt->execute();
         $result = $stmt->fetchAll();
         return $result;
     }
 
     function maxSkillOfUser(){
-        $stmt = $this->$connect->prepare('SELECT u1.name user_name, skill.name as skill_name, MAX(score.score) sk_score FROM user u1 JOIN score ON u1.id = score.user_id LEFT JOIN skill ON score.skill_id = skill.id GROUP BY u1.id, skill.name HAVING sk_score >= (SELECT MAX(score.score) FROM user u2 JOIN score ON u2.id = score.user_id LEFT JOIN skill ON score.skill_id = skill.id WHERE u1.id = u2.id GROUP BY u2.id)');
+        $stmt = $this->$connect->prepare('SELECT u1.name user_name, skill.name as skill_name, MAX(score.score) sk_score 
+        FROM user u1 JOIN score ON u1.id = score.user_id LEFT JOIN skill ON score.skill_id = skill.id 
+        GROUP BY u1.id, skill.name HAVING sk_score >= (SELECT MAX(score.score) FROM user u2 JOIN score ON u2.id = score.user_id LEFT JOIN skill ON score.skill_id = skill.id WHERE u1.id = u2.id GROUP BY u2.id)');
         $stmt->execute();
         $result = $stmt->fetchAll();
         return $result;
     }
 
     function skillFavorite(){
-        $stmt = $this->$connect->prepare('select skill.name skill_name, group_concat(user.name) group_user_name FROM user join score ON user.id = score.user_id LEFT JOIN  skill ON score.skill_id = skill.id GROUP BY skill.id LIMIT 1');
+        $stmt = $this->$connect->prepare('select skill.name skill_name, group_concat(user.name) group_user_name 
+        FROM user join score ON user.id = score.user_id LEFT JOIN  skill ON score.skill_id = skill.id 
+        GROUP BY skill.id HAVING count(*) = (select count(*) as c FROM user join score ON user.id = score.user_id LEFT JOIN  skill ON score.skill_id = skill.id 
+        GROUP BY skill.id ORDER BY c DESC LIMIT 1)');
         $stmt->execute();
         $result = $stmt->fetchAll();
         return $result;
     }
 
     function skillHasSecondPoint(){
-        $stmt = $this->$connect->prepare('select skill.name skill_name, AVG(score.score) avg_score, group_concat(user.name) group_user_name FROM user join score ON user.id = score.user_id LEFT JOIN  skill ON score.skill_id = skill.id GROUP BY skill.id ORDER BY avg_score DESC limit 1,1');
+        $stmt = $this->$connect->prepare('select skill.name skill_name, AVG(score.score) avg_score, group_concat(user.name) group_user_name 
+        FROM user join score ON user.id = score.user_id LEFT JOIN  skill ON score.skill_id = skill.id 
+        GROUP BY skill.id HAVING avg_score = (select AVG(score.score) avg_score
+        FROM user join score ON user.id = score.user_id LEFT JOIN  skill ON score.skill_id = skill.id 
+        GROUP BY skill.id ORDER BY avg_score DESC LIMIT 1,1) ORDER BY avg_score');
         $stmt->execute();
         $result = $stmt->fetchAll();
         return $result;
