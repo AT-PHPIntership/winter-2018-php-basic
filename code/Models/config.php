342f1e8b683d630c5650bfdcb8ecd700de76134b
at-phuongtran <phuong.tran@asiantech.vn>
2018-12-05 13:47:06 +0700

    Exercise 1

---
+++ code/Models/config.php
@@ -0,0 +1,10 @@
+<?php
+
+class Config {
+    const DB_HOST = "mysql";
+    const DB_USERNAME = "root";
+    const DB_PASSWORD= "root";
+    const DB_PORT = "3306";
+    const DB_NAME = "exercise_php";
+}
+?>
