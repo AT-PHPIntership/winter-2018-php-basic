4d37deb7bcad01d99d451e986515c5241fc2c997
leno <lam.do@asiantech.vn>
2018-12-05 08:34:34 +0700

    Create Model to connect DB

---
+++ code/Models/Model.php
@@ -0,0 +1,24 @@
+<?php 
+include "config.php";
+
+class Model extends Config 
+{
+    public $db;
+
+    public function __construct()
+    {
+        $this->db = $this->dbConfig();
+    }
+    public function dbConfig()
+    {
+        try {
+            $conn = new PDO('mysql:host='.Config::DB_HOST.'; port='.Config::DB_PORT.'; dbname='.Config::DB_NAME,
+            Config::DB_USERNAME, Config::DB_PASSWORD);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully"; 
+        } 
+        catch(PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
+}
--- code/Models/config.php
+++ code/Models/config.php
@@ -5,6 +5,6 @@ class Config
     const DB_HOST = 'mysql'; 
     const DB_USERNAME = 'root'; 
     const DB_PASSWORD = 'root'; 
-    const DB_PORT = '3303'; 
-    const DB_NAME = 'learn_php'; 
+    const DB_PORT = '3306'; 
+    const DB_NAME = 'php'; 
 }
